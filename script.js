af83fbfc75eb08cf8d8807677e5ce36fcff865f1
Facundo A. Pestana <136341998+FacuPestana@users.noreply.github.com>
2024-02-22 13:05:31 -0300

    Version 1.4
    Se cambio el nombre de los botones.
    Se añadieron, imagenes, boostrap y se centro los elementos.

--- script.js
+++ script.js
@@ -4,60 +4,49 @@ Crear el boton de reset
 */
 
 let botonElement = document.getElementById("botonClick");
-let botonZarpado = document.getElementById('botonClick2')
+let botonZarpado = document.getElementById("botonClick2");
 let pElement = document.getElementById("areaContador");
-let btnClear = document.getElementById('btnClear')
-let areaContador = document.getElementById('areacontador')
+let btnClear = document.getElementById("btnClear");
+let areaContador = document.getElementById("areacontador");
+
 // De 1 en 1
 let contador = 0;
 botonElement.onclick = function () {
-    //contador++;
-    //De 2 en 2
-    contador ++
+  //contador++;
+  //De 2 en 2
+  contador++;
+  pElement.textContent = contador;
+
+  if (contador > 10) {
+    contador++;
+    //contador = contador + 1
     pElement.textContent = contador;
-   
-   
-    if (contador > 10) {
+  }
 
-        contador++
-        //contador = contador + 1 
-        pElement.textContent = contador;
-    }
+  //De 3 en 3
 
-//De 3 en 3
+  if (contador > 30) {
+    contador++;
+    pElement.textContent = contador;
+  }
+};
 
-    if (contador > 30) {
-
-        contador ++
-        pElement.textContent = contador;
-    }
-}
 botonZarpado.onmousemove = function () {
-    //contador++;
-    //De 2 en 2
-    contador ++
+  //contador++;
+  //De 2 en 2
+  contador++;
+  pElement.textContent = contador;
+
+  if (contador > 10) {
+    contador++;
+    //contador = contador + 1
     pElement.textContent = contador;
-   
-   
-    if (contador > 10) {
-
-        contador++
-        //contador = contador + 1 
-        pElement.textContent = contador;
-    }
-
-//De 3 en 3
-
-    if (contador > 30) {
-
-        contador ++
-        pElement.textContent = contador;
-    }
-}
+  }
+};
 
 //Boton reset del contador
 
-btnClear.onclick= function(){
-    pElement.textContent = 0
-    contador = 0
-}
+btnClear.onclick = function () {
+  pElement.textContent = 0;
+  contador = 0;
+};
